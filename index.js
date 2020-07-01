abb78569c103b7e46d970d7420f4f2d658db6dab
Kenny Davis <kingdavis2423@gmail.com>
2020-07-01 01:49:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+}
+function happyHolidaysTo(name){
+  return "Happy holidays, ${name}!";
+}
 function happyHolidays(holiday, name){
   return "Happy ${holiday}, ${name}!";
 }
